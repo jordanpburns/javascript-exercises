e4ee09cc3c0439f031c947aa7d8ef18c4f58cd56
Jordan Burns <jordanpburns13@gmail.com>
2024-04-11 14:31:46 -0700

    finish exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    word = word.replace(/[^\w]/g, "");
+    word = word.toLowerCase(); 
+    for (i = 0; i < word.length; i += 1) {
+        if (word[i] != word[word.length - i - 1]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
