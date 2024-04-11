eeb5561d7798590c5d22385eefe9f2a7e637b404
Jordan Burns <jordanpburns13@gmail.com>
2024-04-11 14:10:22 -0700

    finish exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(addend1, addend2) {
+	return parseInt(addend1) + parseInt(addend2);
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return parseInt(num1) - parseInt(num2);
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((a, b) => a + b, 0);
 };
 
 const multiply = function() {
-
+  const args = Array.from(arguments);
+  let answer = 1;
+  for (num of args) {
+    answer *= num;
+  }
+  return answer;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	let answer = 1;
+  for (i = 0; i < exponent; i += 1) {
+    answer = answer * base;
+  }
+  return answer;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let answer = 1;
+  for (i = 1; i <= num; i += 1) {
+    answer *= i;
+  }
+  return answer;
 };
 
 // Do not edit below this line
