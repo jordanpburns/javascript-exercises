9db30e94e6574a58ba5784374ab3e402609e48e3
Jordan Burns <jordanpburns13@gmail.com>
2024-04-11 13:31:53 -0700

    finish exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(start, stop) {
+    if (typeof start != "number" || typeof stop != "number" || start < 0 || stop < 0) {
+        return 'ERROR';
+    }
+    let sum = 0
+    const bounds = [start, stop].sort();
+    start = bounds[0];
+    stop = bounds[1];
+    for (i = start; i <= stop; i += 1) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
