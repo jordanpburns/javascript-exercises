29d5d99d143ce1d39d3de1aa18d88e79bb05203a
Jordan Burns <jordanpburns13@gmail.com>
2024-04-11 13:17:53 -0700

    finish exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array) {
+    const args = Array.from(arguments);
+    const toRemove = args.slice(1);
+    for (itemToRemove of toRemove) {
+        array = array.filter((item) => item !== itemToRemove);
+    }
+return array
 };
 
 // Do not edit below this line
