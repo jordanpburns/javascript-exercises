21823b07f47c0493d3f868ee6de373c59e1b3e5b
Jordan Burns <jordanpburns13@gmail.com>
2024-04-11 12:10:17 -0700

    finish exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string, numberOfRepeats) {
+    if (numberOfRepeats < 0) {
+        return 'ERROR';
+    }
+    let returnString = ''
+    for (i = 0; i < numberOfRepeats; i += 1) {
+        returnString += string;
+    }
+    return returnString
+}
 
 // Do not edit below this line
 module.exports = repeatString;
