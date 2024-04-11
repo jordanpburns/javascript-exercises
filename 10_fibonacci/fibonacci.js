6915bd2c06735f7e61439289f8bdb0f645dfe0ed
Jordan Burns <jordanpburns13@gmail.com>
2024-04-11 16:46:02 -0700

    finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n < 0) {
+        return "OOPS"
+    }
+    if (n == 1) {
+        return 1;
+    }
+    else if (n == 2) {
+        return 1;
+    } else {
+        return fibonacci(parseInt(n) - 1) + fibonacci(parseInt(n) - 2);
+    }
 };
 
 // Do not edit below this line
