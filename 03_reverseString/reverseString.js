a79042afba82396b86aa3df5bf209ff772ed7acc
Jordan Burns <jordanpburns13@gmail.com>
2024-04-11 12:31:20 -0700

    finish exercise 4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const stringArray = string.split('');
+    let returnString = '';
+    for (char of stringArray) {
+        returnString = char + returnString;
+    }
+    return returnString;
 };
 
 // Do not edit below this line
