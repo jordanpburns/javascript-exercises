dcddaa023b18adbdbf45d9a6ebd1995a00c37ce0
Jordan Burns <jordanpburns13@gmail.com>
2024-04-11 17:14:33 -0700

    finish exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,27 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldestPerson = people[0];
+    if (Object.hasOwn(oldestPerson, 'yearOfDeath')) {
+        oldestAge = oldestPerson.yearOfDeath - oldestPerson.yearOfBirth;
+    } else {
+        oldestAge = new Date().getFullYear() - oldestPerson.yearOfBirth;
+    }
 
+    for (person of people.slice(1)) {
+        if (Object.hasOwn(person, 'yearOfDeath')) {
+            let personAge = person.yearOfDeath - person.yearOfBirth;
+            if (personAge > oldestAge) {
+                oldestPerson = person;
+                oldestAge = personAge;
+             }
+        } else {
+            personAge = new Date().getFullYear() - person.yearOfBirth;
+            if (personAge > oldestAge) {
+                oldestPerson = person;
+                oldestAge = personAge;
+             }
+        }
+    }
+    return oldestPerson;
 };
 
 // Do not edit below this line
